de893d58760b68dfb82740e9b3d5e497cf044bb6
Enzo <enzotresmediano@gmail.com>
2024-10-24 11:11:41 -0300

    Created simple camera detection, untested

--- cam_corder.py
+++ cam_corder.py
@@ -1,2 +1,26 @@
 import cv2
 import numpy as np
+
+
+# Global variables ---------------------------------------------------------
+capture = cv2.VideoCapture(0)
+if not capture.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    # Capturing image frame-by-frame
+    ret, frame = capture.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+
+    
+
+    # Breaking the loop if the key 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
