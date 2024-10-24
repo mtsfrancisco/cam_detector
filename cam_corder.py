0195219d82b11c3201b204170fb86bb0a25f5737
Enzo <enzotresmediano@gmail.com>
2024-10-24 11:18:00 -0300

    Modularized cam_corder and created area for each integrant to put their methods

--- cam_corder.py
+++ cam_corder.py
@@ -3,17 +3,35 @@ import numpy as np
 
 
 # Global variables ---------------------------------------------------------
-capture = cv2.VideoCapture(0)
-if not capture.isOpened():
-    print("Cannot open camera")
-    exit()
 
+
+# Functions ----------------------------------------------------------------
+def get_video(video_path):
+    capture = cv2.VideoCapture(video_path) # Capture Video
+
+    if not capture.isOpened():
+        print("Error: Could not open video.")
+        exit()
+
+    return capture
+
+
+
+# Main ----------------------------------------------------------------------
+capture = get_video()
 while True:
     # Capturing image frame-by-frame
     ret, frame = capture.read()
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
+
+
+        # Matheus Method // Yolo
+
+        # Cury Method
+
+        # Enzo Method // WaterShed/CountingContours
 
 
     
---
+++ video_processing/water_shed.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+import argparse
+import math
+import imutils
+from matplotlib import pyplot as plt
